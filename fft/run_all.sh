698df91dbb9bfb1aadfa9b3e38927d1d79097b80
James Smith <james.smith9113@gmail.com>
2021-11-06 13:36:49 -0400

    Add comments to help people with MKL problems

--- fft/run_all.sh
+++ fft/run_all.sh
@@ -4,10 +4,13 @@ rm -f _data/bench_fft_data.csv
 
 N_REPEAT=10
 
-for NTHREADS in 1 2 4 8 16
+for NTHREADS in 1 2 4 8
 do
     for _ in $(seq $N_REPEAT)
     do
-        MKL_NUM_THREADS=$NTHREADS OMP_NUM_THREADS=$NTHREADS python bench_fft.py
+	# Some MKL builds may need to preload core and sequential to avoid the error
+	# "undefined symbol: mkl_sparse_optimize_bsr_trsm_i8"
+	# LD_PRELOAD=$MKLROOT/lib/intel64/libmkl_core.so:$MKLROOT/lib/intel64/libmkl_sequential.so \
+	MKL_NUM_THREADS=$NTHREADS OMP_NUM_THREADS=$NTHREADS python bench_fft.py
     done
 done
